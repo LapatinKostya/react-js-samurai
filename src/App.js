d5cf9482f037412e3d8338fa88fd8223f86e6c9e
Kostya Lapatin <klrotex11@gmail.com>
2022-05-10 13:53:57 +0300

    07. Lesson React JS (component)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import './App.css';
 
 function App() {
   return (
-      <div className="App">
-        <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JS</li>
-        </ul>
+      <div>
+          <Header />
+          <Technologies />
       </div>
   );
 }
-
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+            </ul>
+        </div>
+    )
+}
+function Header() {
+    return (
+        <div>
+            <a href={'#s'}>Home</a>
+            <a href={'#s'}>News Feed</a>
+            <a href={'#s'}>Messages</a>
+        </div>
+    )
+}
 export default App;
